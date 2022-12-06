89816f672879cdab32365ce6f7243b527af0d3c9
Jacob Scott <scott.jacob97@gmail.com>
2022-12-05 20:53:17 -0800

    fixes postgres connection issue

--- config.py
+++ config.py
@@ -11,3 +11,10 @@ SECRET_KEY = environ.get("SECRET_KEY")
 SQLALCHEMY_DATABASE_URI = environ.get("DB_URL")
 SESSION_PERMANENT = True
 SESSION_TYPE = "sqlalchemy"
+# hopefully this fixes the issue where pg connection gets GC'd
+# https://stackoverflow.com/questions/58866560/flask-sqlalchemy-pool-pre-ping-only-working-sometimes
+SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_size': 10,
+    'pool_recycle': 60,
+    'pool_pre_ping': True
+}
